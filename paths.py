76952e1474dce8c888740adcb82ab061a41fbfb8
Mikhail Romantsov <miromantsov@yandex.ru>
2024-01-23 03:22:48 +0300

    Rewrite root path.

--- paths.py
+++ paths.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-root = str(Path.cwd()) + "\\..\\"
+root = str(Path.cwd()) + "\\"
 credentials = root + "auth_creds.json"
 reports = root + "reports"
 screenshots = root + "screenshots"
