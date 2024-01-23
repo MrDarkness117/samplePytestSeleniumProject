0d396605b11ed478185ac4443344d644cb8b02cf
Mikhail Romantsov <miromantsov@yandex.ru>
2024-01-23 03:43:25 +0300

    update creds

--- paths.py
+++ paths.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 root = str(Path.cwd()) + "\\"
-credentials = root + "auth_creds.json"
+credentials = root + "creds.json"
 reports = root + "reports"
 screenshots = root + "screenshots"
 selenium_prefs = root + "selenium_prefs.json"
